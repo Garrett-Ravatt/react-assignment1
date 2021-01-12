0073ff60899679ee65cbf2499b2dc43eecd3274c
Garrett-Ravatt <garrbear123456789@gmail.com>
2021-01-11 20:35:21 -0800

    person append form

--- src/MyApp.js
+++ src/MyApp.js
@@ -29,10 +29,14 @@ function MyApp() {
       setCharacters(updated);
     }
 
+    function updateList(person){
+      setCharacters([...characters, person]);
+    }
+
     return (
       <div className="container">
         <Table characterData={characters} removeCharacter={removeOneCharacter} />
-        <Form />
+        <Form handleSubmit={updateList} />
       </div>
     );
 }
